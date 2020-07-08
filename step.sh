51d577d9ad59aeafdf37086aed99dcaad34229e6
Łukasz Łabuński <lukasz@onegini.com>
2020-07-08 14:36:39 +0200

    Added debug flags to called methods and possibility to turn them on and off

--- step.sh
+++ step.sh
@@ -5,16 +5,22 @@ if [[ "${is_debug}" == "yes" ]] ; then
 	set -x
 fi
 
-echo "This is 'onegini_artifactory_username': ${onegini_artifactory_username}"
-echo "This is 'onegini_artifactory_password': ${onegini_artifactory_password}"
-echo "This is 'calculator_version': ${calculator_version}"
-echo "This is 'application_path': ${application_path}"
+curlQuietParam="-sS"
+unzipQuietParam="-q"
+if [[ "${is_debug}" == "yes" ]] ; then
+	curlQuietParam=""
+    unzipQuietParam=""
+fi
+
+echo "Calculator version: ${calculator_version}"
+echo "Application path: ${application_path}"
+echo "Temporary path: ${temporary_path}"
 
 calculator_url="https://repo.onegini.com/artifactory/onegini-sdk/com/onegini/mobile/sdk/ios/ios-app-signature-calculator/${calculator_version}/ios-app-signature-calculator-${calculator_version}.jar"
 calculator_download_path="${temporary_path}/ios-app-signature-calculator-${calculator_version}.jar"
 
-curl -u ${onegini_artifactory_username}:${onegini_artifactory_password} ${calculator_url}  -o ${calculator_download_path}
-unzip -o ${application_path} -d ${temporary_path}
+curl ${curlQuietParam} -u ${onegini_artifactory_username}:${onegini_artifactory_password} ${calculator_url}  -o ${calculator_download_path}
+unzip ${unzipQuietParam} -o ${application_path} -d ${temporary_path}
 unzipped_app_path=$(find ${temporary_path} -name *.app) 
 application_thumbprint=$(java -jar ${calculator_download_path} ${unzipped_app_path} -q)
 envman add --key ONEGINI_APP_THUMBPRINT --value "${application_thumbprint}"
