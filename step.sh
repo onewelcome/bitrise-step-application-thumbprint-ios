af421316a02527ca67c119bed1a375f680989f7e
Łukasz Łabuński <lukasz@onegini.com>
2020-07-08 12:09:15 +0200

    Script cleanup

--- step.sh
+++ step.sh
@@ -6,11 +6,14 @@ echo "This is 'onegini_artifactory_password': ${onegini_artifactory_password}"
 echo "This is 'calculator_version': ${calculator_version}"
 echo "This is 'application_path': ${application_path}"
 
-result="Signature not calculated"
+calculator_url="https://repo.onegini.com/artifactory/onegini-sdk/com/onegini/mobile/sdk/ios/ios-app-signature-calculator/${calculator_version}/ios-app-signature-calculator-${calculator_version}.jar"
+calculator_download_path="${temporary_path}/ios-app-signature-calculator-${calculator_version}.jar"
 
-curl -u ${onegini_artifactory_username}:${onegini_artifactory_password} "https://repo.onegini.com/artifactory/onegini-sdk/com/onegini/mobile/sdk/ios/ios-app-signature-calculator/${calculator_version}/ios-app-signature-calculator-${calculator_version}.jar" -o "${temporary_path}/ios-app-signature-calculator-${calculator_version}.jar"
+curl -u ${onegini_artifactory_username}:${onegini_artifactory_password} ${calculator_url}  -o ${calculator_download_path}
 unzip -o ${application_path} -d ${temporary_path}
-envman add --key ONEGINI_APP_THUMBPRINT --value "$(java -jar ${temporary_path}/ios-app-signature-calculator-${calculator_version}.jar $(find ${temporary_path} -name *.app))"
+unzipped_app_path=$(find ${temporary_path} -name *.app) 
+application_thumbprint=$(java -jar ${calculator_download_path} ${unzipped_app_path} -q)
+envman add --key ONEGINI_APP_THUMBPRINT --value "${application_thumbprint}"
 
 # Envman can handle piped inputs, which is useful if the text you want to
 # share is complex and you don't want to deal with proper bash escaping:
